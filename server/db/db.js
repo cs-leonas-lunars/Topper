7b86ee121ed01ba3e038ab5e5df1cd1674c9e9b7
Jayme Mitchell <jaymetmitchell@gmail.com>
2020-12-04 10:37:55 -0600

    smart contract start and associated files added

--- server/db/db.js
+++ server/db/db.js
@@ -4,13 +4,10 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  'topper',
-  'jaymemitchell',
-  'pencil',
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, // usually is `${databaseName}`
   {
-    host: 'localhost',
-    dialect: 'postgres'
-  } // usually is `${databaseName}`
+    logging: false
+  }
 )
 module.exports = db
 
