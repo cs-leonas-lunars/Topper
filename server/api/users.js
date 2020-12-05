2b5be1d9c7453d5e3fe6ad2cd643cdf53c438bd0
Jess <jessicaspiezio07@gmail.com>
2020-12-05 12:50:32 -0500

    Fixed bugs with routes and transaction page

--- server/api/users.js
+++ server/api/users.js
@@ -15,6 +15,24 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.get('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.username)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// CHECK THIS LOGIC
+router.put('/', async (req, res, next) => {
+  try {
+    await User.update(req.body.account)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.put('/update', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.user.id)
