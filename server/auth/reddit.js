a96caa3985de0a9b3d3530510e235c7318adb43f
Alex Dunne <alex12dunne@gmail.com>
2020-12-05 13:01:39 -0500

    changed port and strat

--- server/auth/reddit.js
+++ server/auth/reddit.js
@@ -44,7 +44,7 @@ router.get('/callback', (req, res, next) => {
   if (req.query.state === req.session.state) {
     passport.authenticate('reddit', {
       failureRedirect: '/',
-      successRedirect: '/home'
+      successRedirect: '/'
     })(req, res, next)
   } else {
     next(new Error(403))
