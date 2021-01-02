60eaa25aba29433780460211e7cffdfda084adff
Jess <jessicaspiezio07@gmail.com>
2021-01-02 13:12:25 -0500

    Fixed loading time

--- client/Transaction.js
+++ client/Transaction.js
@@ -5,25 +5,23 @@ import {loadBlockchainData} from './loadData'
 import {createTransaction} from './userActions'
 
 // transaction component
-const Transaction = (props) => {
+const Transaction = props => {
   const [amount, setAmount] = useState(0)
   const [cancelStatus, setCancelStatus] = useState(0)
   const [data, setData] = useState({transferData: null, loading: true})
 
   useEffect(() => {
-    setTimeout(() => {
-      loadBlockchainData(props.recipient).then((x) => {
-        setData({transferData: x, loading: false})
-      })
-    }, 2000)
+    loadBlockchainData(props.recipient).then(x => {
+      setData({transferData: x, loading: false})
+    })
   }, [])
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault()
     const info = {
       account: data.transferData.account,
       recipient: data.transferData.recipient.address,
-      amount,
+      amount
     }
     if (data.transferData.walletType === 'Metamask') {
       await metaMaskTransaction(info.recipient, info.amount, info.account)
@@ -35,7 +33,7 @@ const Transaction = (props) => {
       senderId: props.user.id,
       amount,
       linkToPost: props.link,
-      platform: props.platform,
+      platform: props.platform
     })
     window.location.replace('http://topper-fsa.herokuapp.com/success')
   }
@@ -101,7 +99,7 @@ const Transaction = (props) => {
               )}
               <input
                 id="setAmountInput"
-                onChange={(e) => setAmount(e.target.value)}
+                onChange={e => setAmount(e.target.value)}
                 type="float"
                 name="amount"
                 autoComplete="off"
