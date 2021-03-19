2e5af4773372b3d1544a2e5264749698fe229ebf
Kevin Zieber <zieber.kevinr@gmail.com>
2021-03-19 09:50:21 -0700

    reverting to previous db.js config

--- server/db/db.js
+++ server/db/db.js
@@ -3,37 +3,12 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-let config
-if (process.env.DATABASE_URL) {
-  config = {
-    logging: false,
-    operatorsAliases: false,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, // usually is `${databaseName}`
+  {
+    logging: false
   }
-} else {
-  config = {
-    logging: false,
-    operatorsAliases: false
-  }
-}
-
-const db = new Sequelize(dbUrl, config)
-
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, // usually is `${databaseName}`
-//   {
-//     logging: false
-//   }
-// )
-
+)
 module.exports = db
 
 if (process.env.NODE_ENV === 'test') {
