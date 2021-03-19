11a8030bb1664277ecae66194ec39818ab0618d1
Kevin Zieber <zieber.kevinr@gmail.com>
2021-03-19 09:45:47 -0700

    changes to db.js to fix deployment

--- server/db/db.js
+++ server/db/db.js
@@ -3,12 +3,37 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, // usually is `${databaseName}`
-  {
-    logging: false
+let config
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    operatorsAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
-)
+} else {
+  config = {
+    logging: false,
+    operatorsAliases: false
+  }
+}
+
+const db = new Sequelize(dbUrl, config)
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, // usually is `${databaseName}`
+//   {
+//     logging: false
+//   }
+// )
+
 module.exports = db
 
 if (process.env.NODE_ENV === 'test') {
